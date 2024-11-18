dca7d0f2463b91452ffe9530785af0d3419367ac
Garrett Girod <girodg@hotmail.com>
2024-11-18 11:45:42 -0500

    day 11

--- Week 4/Day11/Day11/Car.cpp
+++ Week 4/Day11/Day11/Car.cpp
@@ -4,3 +4,18 @@ std::string Car::vehicleInformation()
 {
 	return std::to_string(mModelYear) + " " + mMake + " " + mModel;
 }
+
+void Car::Serialize(std::ostream& outstream, char delim) const
+{
+	outstream << mModelYear << delim << mMake << delim << mModel;
+}
+
+void Car::Deserialize(const std::string& csvData, char delim)
+{
+	std::stringstream carStream(csvData);
+	std::string data;
+	std::getline(carStream, data, delim);
+	mModelYear = std::stoi(data);
+	std::getline(carStream, mMake, delim);
+	std::getline(carStream, mModel, delim);
+}
--- Week 4/Day11/Day11/Day11.cpp
+++ Week 4/Day11/Day11/Day11.cpp
@@ -10,6 +10,10 @@ int main()
 {
     std::cout << "Gone in 60 Seconds!\n";//https://carbuzz.com/features/beyond-eleanor-gone-in-60-seconds-car-names#:~:text=The%20Complete%20List%20Of%20Gone%20In%2060%20Seconds,8%201961%20Porsche%20Speedster%20-%20%22Natalie%22%20More%20items
 
+    Car myRide(1967, "Ford", "Shelby Mustang GT500");
+    std::string path = "garage.csv";
+    std::ofstream outFile(path);
+    char delimiter = '$';
 
     std::vector<Car> garage;
     garage.push_back(Car(1967, "Ford", "Shelby Mustang GT500"));
@@ -17,6 +21,21 @@ int main()
     garage.push_back(Car(1961, "Porsche", "Speedster"));
     garage.push_back(Car(1965, "Pontiac", "GTO"));
     garage.push_back(Car(1969, "Plymouth", "Hemi Cuda"));
+
+    if (outFile.is_open())
+    {
+        //write out the object
+        for (auto& car : garage)
+        {
+            car.Serialize(outFile, delimiter);
+            outFile << "\n";
+        }
+        //myRide.Serialize(outFile, delimiter);
+    }
+    else {
+        std::cout << path << " could not be opened.\n";
+    }
+    outFile.close();
 
 
     /*
@@ -49,4 +68,34 @@ int main()
             split each car to get the car details
 
     */
+    std::ifstream inFile(path);
+    std::vector<Car> newGarage;
+    if (inFile.is_open())
+    {
+        while (!inFile.eof())
+        {
+            std::string line;
+            std::getline(inFile, line);
+
+            if (!line.empty())
+            {
+                Car nextCar(line, delimiter);
+                newGarage.push_back(nextCar);
+            }
+
+        }
+    }
+    else
+    {
+        std::cout << path << " could not be opened.\n";
+    }
+    inFile.close();
+
+    std::cout << "\n\nMy Garage\n";
+    for (auto& car : newGarage)
+    {
+        car.Serialize(std::cout, ' ');
+        std::cout << "\n";
+        //std::cout << car.ModelYear() << " " << car.Make() << " " << car.Model() << "\n";
+    }
 }
